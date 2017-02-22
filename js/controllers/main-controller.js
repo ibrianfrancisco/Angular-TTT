33bd297d9d81ab717d873a1c018a3c57b97b12df
Brian Francisco <ibrianfrancisco@gmail.com>
2017-02-21 21:30:27 -0800

    added cat game logic

--- js/controllers/main-controller.js
+++ js/controllers/main-controller.js
@@ -14,6 +14,20 @@ function MainController($scope, $state) {
                   ['','','',],
                   ['','','']];
 
+  function changeTurn() {
+    $scope.turnNumber === 0 ? $scope.turnNumber = 1 : $scope.turnNumber = 0
+  }
+
+  function checkWinner(player) {
+    $state.go('gameover');
+    $scope.winner = player + " wins!";
+  }
+
+  function catsGame() {
+    $state.go('gameover');
+    $scope.winner = "Cats Game :["
+  }
+
   $scope.makeChoice = function(row, column) {
     if ($scope.board[row][column] === '') {
       var choice = ($scope.turnNumber === 0) ? 'X' : 'O';
@@ -21,72 +35,6 @@ function MainController($scope, $state) {
       changeTurn();
       checkWin();
     }
-
-  }
-
-  function changeTurn() {
-    $scope.turnNumber === 0 ? $scope.turnNumber = 1 : $scope.turnNumber = 0
-  }
-
-  function checkWin() {
-    for(var i = 0; i < 3; i++) {
-      // checks if horizontal rows are equal to each other and not equal to an empty string.
-      if($scope.board[i][0] == $scope.board[i][1] && $scope.board[i][2] == $scope.board[i][0] && $scope.board[i][0] !== "" && $scope.board[i][0] == "X"){
-        console.log("X win horizontal");
-        p1Wins();
-      }
-      else if ($scope.board[i][0] == $scope.board[i][1] && $scope.board[i][2] == $scope.board[i][0] && $scope.board[i][0] !== "" && $scope.board[i][0] == "O"){
-        console.log("O win horizontal");
-        p2Wins();
-      }
-      // checks if vertical columns are equal to each other and not equal to an empty string.
-      else if ($scope.board[0][i] == $scope.board[1][i] && $scope.board[2][i] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "X") {
-        console.log("X win vertical");
-        p1Wins();
-      }
-      else if ($scope.board[0][i] == $scope.board[1][i] && $scope.board[2][i] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "O") {
-        console.log("O win vertical");
-        p2Wins();
-      // checks diagonal. only true when i = 0
-      }
-      else if ($scope.board[0][i] == $scope.board[1][i+1] && $scope.board[2][i +2] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "X") {
-        console.log("X win diagonal right");
-        p1Wins();
-      }
-      else if ($scope.board[0][i] == $scope.board[1][i+1] && $scope.board[2][i +2] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "O") {
-        console.log("O win diagonal right");
-        p2Wins();
-      }
-      //checks left diagonal. only true when i = 2
-      else if ($scope.board[0][i] == $scope.board[1][i-1] && $scope.board[2][i-2] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "X") {
-        console.log("X win diagonal left");
-        p1Wins();
-      }
-      else if ($scope.board[0][i] == $scope.board[1][i-1] && $scope.board[2][i-2] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "O") {
-        console.log("O win diagonal left");
-        p2Wins();
-      }
-      // if turnNumber reaches 9 there's been no winner, tie condition is enforced.
-      else if ($scope.turnNumber == 9) {
-        catsGame();
-      }
-    }
-  }
-
-
-  function p1Wins() {
-    $state.go('gameover');
-    $scope.winner = $scope.player1 + " wins!"
-  }
-
-  function p2Wins() {
-    $state.go('gameover');
-    $scope.winner = $scope.player2 + " wins!"
-  }
-
-  function catsGame() {
-    $state.go('gameover');
-    $scope.winner = "Cats Game :["
   }
 
   $scope.goToBoard = function(playerOne, playerTwo) {
@@ -100,6 +48,52 @@ function MainController($scope, $state) {
     $scope.board = [['','','',],
                     ['','','',],
                     ['','','']];
+  }
+
+  function checkWin() {
+    for(var i = 0; i < 3; i++) {
+      // checks if horizontal rows are equal to each other and not equal to an empty string.
+      if($scope.board[i][0] == $scope.board[i][1] && $scope.board[i][2] == $scope.board[i][0] && $scope.board[i][0] !== "" && $scope.board[i][0] == "X"){
+        console.log("X win horizontal");
+        checkWinner($scope.player1);
+      }
+      else if ($scope.board[i][0] == $scope.board[i][1] && $scope.board[i][2] == $scope.board[i][0] && $scope.board[i][0] !== "" && $scope.board[i][0] == "O"){
+        console.log("O win horizontal");
+        checkWinner($scope.player2);
+      }
+      // checks if vertical columns are equal to each other and not equal to an empty string.
+      else if ($scope.board[0][i] == $scope.board[1][i] && $scope.board[2][i] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "X") {
+        console.log("X win vertical");
+        checkWinner($scope.player1);
+      }
+      else if ($scope.board[0][i] == $scope.board[1][i] && $scope.board[2][i] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "O") {
+        console.log("O win vertical");
+        checkWinner($scope.player2);
+      // checks diagonal. only true when i = 0
+      }
+      else if ($scope.board[0][i] == $scope.board[1][i+1] && $scope.board[2][i +2] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "X") {
+        console.log("X win diagonal right");
+        checkWinner($scope.player1);
+      }
+      else if ($scope.board[0][i] == $scope.board[1][i+1] && $scope.board[2][i +2] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "O") {
+        console.log("O win diagonal right");
+        checkWinner($scope.player2);
+      }
+      //checks left diagonal. only true when i = 2
+      else if ($scope.board[0][i] == $scope.board[1][i-1] && $scope.board[2][i-2] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "X") {
+        console.log("X win diagonal left");
+        checkWinner($scope.player1);
+      }
+      else if ($scope.board[0][i] == $scope.board[1][i-1] && $scope.board[2][i-2] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "O") {
+        console.log("O win diagonal left");
+        checkWinner($scope.player2);
+      }
+      else if (($scope.board[0][0] !== '') && ($scope.board[0][1] !== '') && ($scope.board[0][2] !== '') &&
+               ($scope.board[1][0] !== '') && ($scope.board[1][1] !== '') && ($scope.board[1][2] !== '') &&
+               ($scope.board[2][0] !== '') && ($scope.board[2][1] !== '') && ($scope.board[2][2] !== '')) {
+        catsGame();
+      }
+    }
   }
 }
 
